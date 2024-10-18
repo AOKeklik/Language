6980404cd3e10f7b7ca2f9d4c81365fad8895ac8
Hedea Onur <onur@hedea.com>
2024-10-18 15:16:45 +0200

    first commit

---
+++ Polish/Adverbs.js
@@ -0,0 +1,17 @@
+/*
+
+
+* Natychmiast               - derhal, hemen
+* Bezzwłocznie              - derhal, gecikmeden
+* Od razu                   - hemen, hemen hemen
+* Natychmiastowo            - derhal, hemen
+* Błyskawicznie             - hızlı bir şekilde, derhal
+* Pilnie                    - acil olarak, hemen
+* Szybko                    - çabuk, hızlı bir şekilde
+* Teraz                     - şimdi, hemen
+* Niezwłocznie              - derhal, acilen
+
+* również                   - ayrıca, de
+* także                     - ayrıca, de
+* jeszcze                   - henüz, ayrıca
+* oprócz                    - dışında, ayrıca
---
+++ Polish/Verbs.js
@@ -0,0 +1,55 @@
+/*
+
+* Wchodzić              - girmek
+* Wstępować             - giriş yapmak
+* Zachodzić             - girmek, girmeye yaklaşmak
+* Przechodzić           - geçmek, girmek
+* Wchodzić do           - -e girmek (bir yere)
+
+* Wychodzić             - çıkmak
+* Wyjść                 - çıkmak, gitmek (tamamlanmış eylem)
+* Opuszczać             - terk etmek, çıkmak
+* Znikać                - kaybolmak, çıkmak (bir yerden)
+* Wynikać               - sonuçlanmak, çıkmak (bir durumdan)
+
+* Iść                   - gitmek
+* Chodzić               - yürümek, gitmek (düzenli olarak)
+* Odchodzić             - ayrılmak, gitmek
+* Podróżować            - seyahat etmek, gitmek
+
+* Przyjść               - gelmek
+* Nadchodzić            - yaklaşmak, gelmek
+* Przybywać             - varmak, gelmek
+* Zjawiać się           - ortaya çıkmak, gelmek
+
+* Pojawić się           - belirmek, ortaya çıkmak
+* Ukazać się            - görünmek, ortaya çıkmak
+* Wynurzyć się          - su yüzüne çıkmak, ortaya çıkmak
+* Zjawiać się           - ortaya çıkmak, gelmek
+* Wyłonić się           - çıkmak, ortaya çıkmak
+
+* Brać                  - almak
+* Zabierać              - almak, almak üzere götürmek
+* Odbierać              - teslim almak, geri almak
+* Dostać                - almak, elde etmek
+* Przyjąć               - kabul etmek, almak 
+
+* Dawać                 - vermek
+* Przekazywać           - iletmek, vermek
+* Udzielać              - vermek, sağlamak
+* Oddać                 - geri vermek, vermek
+* Zafundować            - hediye etmek, sunmak
+* złożyć            - vermek, teslim etmek
+
+* Zapewniać             - sağlamak
+* Dostarczać            - tedarik etmek, sağlamak
+* Umożliwiać            - olanak tanımak, sağlamak
+* Gwarantować           - garanti etmek, sağlamak
+* Zarządzać             - yönetmek, sağlamak (belirli bir düzen içinde)
+
+* Wspierać              - desteklemek
+* Pomagać               - yardımcı olmak, desteklemek
+* Udzielać wsparcia     - destek sağlamak
+* Zasilać               - güçlendirmek, desteklemek (mecazi anlamda)
+* Sponsoring            - sponsorluk yapmak, desteklemek
+
