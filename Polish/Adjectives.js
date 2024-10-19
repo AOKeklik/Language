263bcbff2ded90273ae8a45fb97c0f5ee248f6fd
Abdullah Onur Keklik <ao_keklik@hotmail.com>
2024-10-19 06:29:28 +0200

    add adjective file

---
+++ Polish/Adjectives.js
@@ -0,0 +1,13 @@
+/*
+
+* Piękny                - guzel
+* Uroczy                - sirin
+* Cudowny               - harika
+* Zjawiskowy            - olaganustu
+* Wspaniały             - olaganustu
+
+* Brzydki               - çirkin
+* Koszmarny             - korkunç, berbat
+* Okropny               - berbat, kötü
+* Nieszczęsny           - talihsiz, şanssız
+* Nieprzyjemny          - hoş olmayan, rahatsız edici
